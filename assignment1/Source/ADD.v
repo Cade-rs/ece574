eb26224efd2cdce708d01da4e641a4f4d99ba295
Cade-rs <Cader.stephenson@gmail.com>
2021-09-19 15:35:45 -0700

    corrections after synthesize tests

--- assignment1/Source/ADD.v
+++ assignment1/Source/ADD.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module ADD #(parameter DATAWIDTH = 8)(a, b);
+module ADD #(parameter DATAWIDTH = 8)(a, b, sum);
     input [DATAWIDTH-1:0] a, b;
     output reg [DATAWIDTH-1:0] sum;
     
