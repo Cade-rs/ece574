533dd51f3e86f43231994df28cf754d1f107cb02
Cade-rs <Cader.stephenson@gmail.com>
2021-10-02 16:30:46 -0700

    Update .py script comments

--- misc/latencyParser.py
+++ misc/latencyParser.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Parse timing reports for component latencies and write to DPCL_LAT.txt file
 """
 
 import re
@@ -11,16 +9,15 @@ import os
 dir1 = "C:\\Users\\cader\\Documents\\school\\ECE574\\workspace\\git\\ece574\\assignment1\\Timing"
 os.chdir(dir1)
 
-#
 comps = ['REG','ADD','SUB','MUL','COMP','MUX2x1','SHR','SHL','DIV','MOD','INC','DEC']
 nbits = ['2','8','16','32','64']
 
-culprit = 'Data Path Delay:'     
 regexp = re.compile(r'Data Path Delay:.*?([0-9.-]+)')
 txt = ''
 
 for comp in comps:
     
+    #reset latencies array
     latency = []
     for nbit in nbits:
         fname = 'timing' + comp + nbit + '.txt'
@@ -32,16 +29,8 @@ for comp in comps:
     
     #form appendix
     txt = txt + comp+' : '+latency[0]+', '+latency[1]+', '+latency[2]+', '+latency[3]+', '+latency[4]+'\n'
-    
-            #for line in f:
-            #    match = regexp.match(line)
-            #   if match:
-            #        latency = match.group(1)
-            #        print(latency)
-                    
-        #print re.search(r'Data Path Delay:.*?=,(.+)', f.read()).group(1)                
-        #print re.search(' Data Path Delay:*', f.read())                
-    
+                 
+#create text file (wrong directory though)
 file = open('DPCL_LAT.txt', "w")
 
 file.write(txt)
