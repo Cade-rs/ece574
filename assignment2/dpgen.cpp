853fcb44a8bbcfc14da0e4c151f833d74d127086
Brandon Kral <bmkral@gmail.com>
2021-10-20 21:03:06 -0700

    Add file read and command line args

--- assignment2/dpgen.cpp
+++ assignment2/dpgen.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, char *argv[])
 {
+    std::string inFile  = argv[1];
+    std::string outFile = argv[2];
+    std::string line;
+
+    std::ifstream fin;
+    std::ofstream fout;
+
+    fin.open(inFile);
+    fout.open(outFile);
+    
+    if ( !fin.is_open() )
+    {
+        std::cout << "Unable to read file: " << inFile << "\n";
+        return 1;
+    }
+    else if ( !fout.is_open() )
+    {
+        std::cout << "Unable to read file: " << outFile << "\n";
+        return 1;
+    }
+    else
+    {
+        std::cout << inFile << " going to " << outFile << "\n";
+    }
+
+    while( getline(fin, line) )
+    {
+        std::cout << line << "\n";
+        // Strip out comments
+
+        // Hardcode the different key words or characters
+        
+    }
 
     return 0;
 }
-
