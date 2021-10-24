e4c83a546a9e9e956967d21c9c5ab356b3b084b3
Cade-rs <Cader.stephenson@gmail.com>
2021-10-23 17:31:12 -0700

    component class and testing complete and compiles

---
+++ assignment2/crstephenson/build/CMakeFiles/pleaseWork.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/cader/Documents/school/ECE574/workspace/git/ece574/assignment2/crstephenson/src/component.cpp" "CMakeFiles/pleaseWork.dir/src/component.cpp.obj" "gcc" "CMakeFiles/pleaseWork.dir/src/component.cpp.obj.d"
+  "C:/Users/cader/Documents/school/ECE574/workspace/git/ece574/assignment2/crstephenson/src/main.cpp" "CMakeFiles/pleaseWork.dir/src/main.cpp.obj" "gcc" "CMakeFiles/pleaseWork.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
