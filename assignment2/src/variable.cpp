74ad92129a729d00feeb5a5b426b699243a8ad0a
Brandon Kral <bmkral@gmail.com>
2021-10-24 17:42:11 -0700

    Added signed flag to variable.*

--- assignment2/src/variable.cpp
+++ assignment2/src/variable.cpp
@@ -2,15 +2,17 @@
 
 #include "latency.h"
 
-variable::variable(std::string name, int size)
+variable::variable(std::string name, int size, bool isSigned)
 {
     name_ = name;
     size_ = int2size(size);
+    isSigned_ = isSigned;
 }
-variable::variable(std::string name, comp_size size)
+variable::variable(std::string name, comp_size size, bool isSigned)
 {
     name_ = name;
     size_ = size;
+    isSigned_ = isSigned;
 }
 
 // Copy constructor
@@ -18,6 +20,7 @@ variable::variable(const variable& in_var)
 {
     name_ = in_var.name_;
     size_ = in_var.size_;
+    isSigned_ = in_var.isSigned_;
 }
 
 bool variable::operator== (std::string param)
--- assignment2/src/variable.h
+++ assignment2/src/variable.h
@@ -8,14 +8,15 @@
 
 class variable {
 public:
-    variable(std::string name, int size);
-    variable(std::string name, comp_size size);
+    variable(std::string name, int size, bool isSigned=false);
+    variable(std::string name, comp_size size, bool isSigned=false);
     variable(const variable& in_var);
     bool operator== (std::string param);
     bool operator== (variable param);
 
     std::string name_;
     comp_size size_;
+    bool isSigned_;
 };
 
 
