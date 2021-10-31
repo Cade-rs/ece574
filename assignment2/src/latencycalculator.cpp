b06341b18fe5ae8bf487ce0cbf337208d08c4da3
Cade-rs <Cader.stephenson@gmail.com>
2021-10-31 15:53:16 -0700

    Debugging version of latencycalculator. Works! Also fix Component bug

--- assignment2/src/latencycalculator.cpp
+++ assignment2/src/latencycalculator.cpp
@@ -10,12 +10,18 @@
 latencycalculator::latencycalculator(){
     
     criticalpath_ = 0.00;
-    delays_.push_back( 0.0 );
+    delays_.push_back( -1.0 );
 
-    std::cout << "initial size = " << std::to_string(delays_.size() ) << std::endl;
-
+    //build the test component list
     std::vector<component> complist {examplecomps()};
+    
+    //Find 
     sumPathLatencies(complist);
+
+    //determine and report critical path
+    criticalpath_ = *max_element(delays_.begin(), delays_.end());
+
+    std::cout << "The critical path is " << std::to_string(criticalpath_) << std::endl;
 }
 
 void latencycalculator::sumPathLatencies(std::vector<component> complist){
@@ -25,48 +31,59 @@ void latencycalculator::sumPathLatencies(std::vector<component> complist){
     //loop through each component
     for(int i=0; i < complist.size(); i++)
     {   
-        std::cout << "branch = " << std::to_string(branch) << "delay size = " << std::to_string(delays_.size()) << std::endl;
+        //std::cout << "branch = " << std::to_string(branch) << "delay size = " << std::to_string(delays_.size()) << std::endl;
 
         recursivesearch(complist, i, branch);
 
-        //for each new component, declare new branch. zero-based so set new branch to size
-        branch = delays_.size();
+        //for each new component (unless finished), declare new branch. zero-based so set new branch to one less than size. Negative number was for debugging purposes
+        if( i < complist.size() -1 )
+        {
+            delays_.push_back( -1.0 );
+            branch = delays_.size()-1;
+        }
+
+        //std::cout << "Done w component, branch = " << std::to_string(branch) << "delay size = " << std::to_string(delays_.size()) << std::endl;
     }
 
-    //determine critical path
-    criticalpath_ = *max_element(delays_.begin(), delays_.end());
-    
-    
-    /////////// DEBUGGING ///////////
-
-    std::cout << "The critical path is " << std::to_string(criticalpath_) << std::endl;
-
-    for (int i = 0; i < delays_.size(); i++)
+    for(int i=0; i<delays_.size(); i++)
     {
-        std::cout << "delay = " << std::to_string(delays_[i]) << std::endl;
+        std::cout << std::endl << std::to_string(delays_[i]) << std::endl;
     }
-
 }
 
 void latencycalculator::recursivesearch(std::vector<component> complist, int compnum, int branch){
-    
+    double currentlat;
     vector<int> matches;    
 
-    //add component delay
-    delays_[branch] += complist[compnum].lat_;
+    //std::cout << "Recurse, branch = " << std::to_string(branch) << "delay size = " << std::to_string(delays_.size()) << std::endl;
+
+    //Update branch number if necessary (OBE: this was primarily for debugging)
+    if (delays_.back() <= 0.0)
+    {
+        delays_[branch] = 0.0;
+    }
     
-    std::cout << std::to_string(complist[compnum].lat_) << std::endl;
+    //std::cout << "updating lat, branch = " << std::to_string(branch) << "delay size = " << std::to_string(delays_.size()) << std::endl;
+
+    //add component delay and store off for recursive purposes later
+    delays_[branch] += complist[compnum].lat_;
+    currentlat = delays_[branch];
+
+    //std::cout << std::to_string(complist[compnum].lat_) << std::endl;
+    //std::cout << complist[compnum].type_ << "   " << std::to_string(delays_[branch]) << std::endl;
 
     //Find components that uses output as input?
     for(int i=0; i<complist.size(); i++)
     {
+        //loop through outputs
         for(int outnum=0; outnum < complist[compnum].out_.size(); outnum++)
         {    
             if( complist[i].containsInput(complist[compnum].out_[outnum].name_) )
             {
-                //TODO: add check for multiple outputs used at same component -- don't double count
-                
                 matches.push_back( i );
+
+                //exit output loop to avoid double counting component that uses two or more of the source nodes outputs
+                break;
             }
         }
     }
@@ -74,26 +91,32 @@ void latencycalculator::recursivesearch(std::vector<component> complist, int com
     //is matches empty? If so, done with path
     if( matches.size() == 0)
     {
+        //std::cout << "empty matches, branch = " << std::to_string(branch) << "delay size = " << std::to_string(delays_.size()) << std::endl;
+
         return;
     }
 
     //Recurse!
     for (int branchiter = 0; branchiter < matches.size(); branchiter++)
     {
-            std::cout << "got here 20" << std::endl;
-
+        //std::cout << "got here 20" << std::endl;
+        //std::cout << "Recurse further1, branch = " << std::to_string(branch) << "delay size = " << std::to_string(delays_.size()) << std::endl;
+        
         //new branch
         if( branchiter > 0)
         {
             //copy current branchs delay and update branch number
-            delays_.push_back( delays_[branch] );
-            branch = delays_.size();
+            delays_.push_back( currentlat );
+            branch = delays_.size()-1;
+            //std::cout << "copying lat to new branch" << std::endl;
         }
+        
+        //std::cout << "Recurse further2, branch = " << std::to_string(branch) << "delay size = " << std::to_string(delays_.size()) << std::endl;
 
         recursivesearch(complist, matches[branchiter], branch);
-        std::cout << "got here 30" << std::endl;
     }
 
+    return;
 }
 
 
