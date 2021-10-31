da2fac50843851754b2b3b1a6ebe6cff079a868c
Cade-rs <Cader.stephenson@gmail.com>
2021-10-30 17:03:16 -0700

    latencycalc take 2. Issue exists with path searching

--- assignment2/buildall.sh
+++ assignment2/buildall.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 #NOTE: need to be in the build directory in order for this to run properly
-pwd
 rm -r ../build/*
-pwd
+
 cmake -G "MinGW Makefiles" ..
-pwd
-mingw32-make
+
+mingw32-make 
--- assignment2/src/latencycalculator.cpp
+++ assignment2/src/latencycalculator.cpp
@@ -1,44 +1,94 @@
 #include "latencycalculator.h"
 
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <iostream> //for flush
 
-#include "latency.h"
 #include "component.h"
-
+#include "latency.h"
 
 latencycalculator::latencycalculator(){
     
     criticalpath_ = 0.00;
+    delays_.push_back( 0.0 );
 
-    const std::vector<component> complist {examplecomps()};
+    std::vector<component> complist {examplecomps()};
     sumPathLatencies(complist);
 }
 
-void latencycalculator::sumPathLatencies(const std::vector<component> complist){
+void latencycalculator::sumPathLatencies(std::vector<component> complist){
 
-    vector<double> pathdelays;
-    int pathnum = 0;
-    bool pathopen = false;
+    int branch = 0;
 
     //loop through each component
     for(int i=0; i < complist.size(); i++)
-    {
-        //add component delay
-        
-        //is there another component that uses output as input?
-
-            //yes - go to that component
-
-            //no - break, you've found that path's delay
+    {   
+        recursivesearch(complist, i, branch);
     }
 
     //determine critical path
+    criticalpath_ = *max_element(delays_.begin(), delays_.end());
+    
+    
+    /////////// DEBUGGING ///////////
+
+    std::cout << "The critical path is " << std::to_string(criticalpath_) << std::endl;
+
+    for (int i = 0; i < delays_.size(); i++)
+    {
+        std::cout << "delay = " << std::to_string(delays_[i]) << std::endl;
+    }
 
 }
 
-//double latencycalculator::recursivesearch(){}
+void latencycalculator::recursivesearch(std::vector<component> complist, int compnum, int branch){
+    
+    vector<int> matches;    
+
+    //add component delay
+    delays_[branch] += complist[compnum].lat_;
+    
+    std::cout << std::to_string(complist[compnum].lat_) << std::endl;
+
+    //Find components that uses output as input?
+    for(int i=0; i<complist.size(); i++)
+    {
+        for(int outnum=0; outnum < complist[compnum].out_.size(); outnum++)
+        {    
+            if( complist[i].containsInput(complist[compnum].out_[outnum].name_) )
+            {
+                //TODO: add check for multiple outputs used at same component -- don't double count
+                
+                matches.push_back( i );
+            }
+        }
+    }
+    
+    //is matches empty? If so, done with path
+    if( matches.size() == 0)
+    {
+        return;
+    }
+
+    //Recurse!
+    for (int branchiter = 0; branchiter < matches.size(); branchiter++)
+    {
+            std::cout << "got here 20" << std::endl;
+
+        //new branch
+        if( branchiter > 0)
+        {
+            //copy current branchs delay and update branch number
+            delays_.push_back( delays_[branch] );
+            branch = delays_.size();
+        }
+
+        recursivesearch(complist, matches[branchiter], branch);
+        std::cout << "got here 30" << std::endl;
+    }
+
+}
+
 
 //build test vector of components for building latency calculator using
 // 474_circuit1.txt
@@ -110,9 +160,10 @@ std::vector<component> latencycalculator::examplecomps(){
     component temp7(comp_type::REG,comp_size::SIXTEEN,in,out);
     complist.push_back(temp7);
 
-    std::cout << complist[4].in_[1].name_ << std::endl << std::endl;
+    //std::cout << complist[4].in_[1].name_ << std::endl << std::endl;
     //std::cout << in[0].name_ << std::endl;
-    std::cout << complist.size() << std::endl;
+    //std::cout << complist.size() << std::endl;
+    //std::cout << complist[4].findLatency() << std::endl;
 
     return complist;
 }
