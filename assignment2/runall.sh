867fbe09f904f3a75e248ff98c2da17a00997b7b
Brandon Kral <bmkral@gmail.com>
2021-11-02 22:51:28 -0700

    Create register component, add runall.sh

---
+++ assignment2/runall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#NOTE: need to be in the build directory in order for this to run properly
+
+./pleaseWork.exe ../assignment_2_circuits/474a_circuit1.txt  ../output/474a_circuit1_out.txt
+./pleaseWork.exe ../assignment_2_circuits/474a_circuit2.txt  ../output/474a_circuit2_out.txt
+./pleaseWork.exe ../assignment_2_circuits/474a_circuit3.txt  ../output/474a_circuit3_out.txt
+./pleaseWork.exe ../assignment_2_circuits/474a_circuit4.txt  ../output/474a_circuit4_out.txt
+./pleaseWork.exe ../assignment_2_circuits/574a_circuit5.txt  ../output/574a_circuit5_out.txt
+./pleaseWork.exe ../assignment_2_circuits/574a_circuit6.txt  ../output/574a_circuit6_out.txt
+
+./pleaseWork.exe ../assignment_2_circuits/error1.txt  ../output/error1_out.txt
+./pleaseWork.exe ../assignment_2_circuits/error2.txt  ../output/error2_out.txt
+./pleaseWork.exe ../assignment_2_circuits/error3.txt  ../output/error3_out.txt
+./pleaseWork.exe ../assignment_2_circuits/error4.txt  ../output/error4_out.txt
+
+./pleaseWork.exe ../assignment_2_circuits/mixedcircuit1.txt  ../output/mixedcircuit1_out.txt
+./pleaseWork.exe ../assignment_2_circuits/mixedcircuit2.txt  ../output/mixedcircuit2_out.txt
+./pleaseWork.exe ../assignment_2_circuits/mixedcircuit3.txt  ../output/mixedcircuit3_out.txt
+
+./pleaseWork.exe ../assignment_2_circuits/ucircuit1.txt  ../output/ucircuit1_out.txt
+./pleaseWork.exe ../assignment_2_circuits/ucircuit2.txt  ../output/ucircuit2_out.txt
+./pleaseWork.exe ../assignment_2_circuits/ucircuit3.txt  ../output/ucircuit3_out.txt
